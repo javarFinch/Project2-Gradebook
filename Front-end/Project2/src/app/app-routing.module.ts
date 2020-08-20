9164638d43cac3f049124b30c9a360a7a866de61
veitej <jkveitengruber@gmail.com>
2020-08-19 18:26:47 -0700

    started routing a little

--- Front-end/Project2/src/app/app-routing.module.ts
+++ Front-end/Project2/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'student',
+    component: StudentComponent,
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
