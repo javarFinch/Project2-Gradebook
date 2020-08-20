9ab3e36ca2183754417676a346417399b22dbad1
ErickHdez1997 <Erick.Hdez199721@gmail.com>
2020-08-19 21:44:17 -0400

    Added the Dao and DaoImpl and some more configuration stuff. Also wrote log in method

--- Back-end/src/main/java/Main.java
+++ Back-end/src/main/java/Main.java
@@ -1,29 +1,21 @@
-import com.ex.Services.SessionFactoryHelper;
-import org.hibernate.*;
-
-import java.util.List;
+import com.ex.Dao.Dao;
+import com.ex.Dao.DaoImpl;
+import com.ex.Models.UsersEntity;
 
 public class Main {
 
     public static void main(String args[])
     {
-        SessionFactoryHelper s = new SessionFactoryHelper();
-        Session session = s.getSessionFactory().openSession();
-        Transaction tx = null;
-        try{
-            tx=session.beginTransaction();
-            SQLQuery query = session.createSQLQuery("select first_name from admin where id = 1");
-            //Query query = session.createQuery("from AdminEntity");
-            System.out.println("we are in the session");
-            System.out.println(query.list());
-//            Query query = session.createQuery(); //You will get Weayher object
-            tx.commit();
-        }catch(HibernateException e){
-            if(tx != null) tx.rollback();
-            e.printStackTrace();
-        }finally{
-            session.close();
-        }
+        Dao dao = new DaoImpl();
+        UsersEntity user = (dao.logIn(3, "password"));
+        System.out.println(user.getId());
+        System.out.println(user.getFirstName());
+        System.out.println(user.getLastName());
+        System.out.println(user.getPassword());
+        System.out.println(user.getType());
+
+        //System.out.println(dao.getClassForStudent());
+        //System.out.println(dao.getAssignmentsForStudentPerClass());
     }
 }
 
